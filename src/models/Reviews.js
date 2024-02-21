39702327423600fb8bc594f95f00b5a4e8aa8f16
Irfat <imirfat10@gmail.com>
2024-02-21 23:41:50 +0600

    product review api added

--- src/models/Reviews.js
+++ src/models/Reviews.js
@@ -36,9 +36,12 @@ const reviewsSchema = mongoose.Schema({
 reviewsSchema.pre("save", async function (next) {
   try {
     const Reviews = this.constructor;
+    console.log("orderId:", this.orderId);
+    console.log("userId:", this.userId);
+    console.log("productId:", this.productId);
 
     //valid order check
-    const validOrder = await Orders.find({
+    const validOrder = await Orders.findOne({
       _id: this.orderId,
       userId: this.userId,
       "productInfo.productId": this.productId,
@@ -57,12 +60,13 @@ reviewsSchema.pre("save", async function (next) {
       throw new Error("Product does not exist");
     }
 
-    const alreadyReviewed = await Reviews.find({
+    const alreadyReviewed = await Reviews.findOne({
       orderId: this.orderId,
       userId: this.userId,
       productId: this.productId,
     });
     if (alreadyReviewed) {
+      console.log("alreadyReviewed");
       await updateUserReviewList(this.userId, alreadyReviewed._id);
       await updateProductReviewList(this.productId, alreadyReviewed._id);
       await updateReviewStatus(this.orderId, this.productId);
@@ -75,11 +79,11 @@ reviewsSchema.pre("save", async function (next) {
   }
 });
 
-reviewsSchema.post("save", async function (next) {
+reviewsSchema.post("save", async function (doc, next) {
   try {
     await updateUserReviewList(this.userId, doc._id);
-    await updateProductReviewList(this.productId, doc._id);
-    await updateReviewStatus(this.orderId, this.productId);
+    await updateProductReviewList(this.productId, doc._id, this.starCount);
+    await updateReviewStatus(this.orderId, doc.productId);
     next();
   } catch (error) {
     next(error);
--- src/utils/productUtils.js
+++ src/utils/productUtils.js
@@ -5,15 +5,33 @@ const checkProductIdExists = async (productId) => {
   return product;
 };
 
-const updateProductReviewList = async (productId, reviewId) => {
+const updateProductReviewList = async (
+  productId,
+  reviewId,
+  newReviewRating = 0
+) => {
   const product = await Products.findById(productId);
   if (product) {
     if (!product.reviews.includes(reviewId)) {
+      const averageRating = calculateNewAverage(
+        product.averageRating,
+        product.reviews.length,
+        newReviewRating
+      );
+      product.averageRating = averageRating;
       product.reviews.push(reviewId);
       await product.save();
     }
   }
   return true;
+};
+
+const calculateNewAverage = (avgRating, totalReviews, newReviewRating) => {
+  const existingTotalRating = avgRating * totalReviews;
+  const newTotalRating = existingTotalRating + newReviewRating;
+  const updatedTotalReviews = totalReviews + 1;
+  const newAvgRating = newTotalRating / updatedTotalReviews;
+  return newAvgRating.toFixed(1);
 };
 
 module.exports = {
