60ea2840a8e43d36a9acfb267c69223e35b261a3
Irfat <imirfat10@gmail.com>
2024-03-01 00:57:00 +0600

    delete coupon added

--- src/controllers/couponsController.js
+++ src/controllers/couponsController.js
@@ -21,3 +21,26 @@ exports.addNewCoupon = async (req, res) => {
     res.status(500).send({ error: true, message: "Internal Server Error" });
   }
 };
+
+//delete existing coupon
+exports.deleteCoupon = async (req, res) => {
+  try {
+    const couponId = req.params.couponId;
+    const deletedCoupon = await Coupons.findOneAndDelete({ _id: couponId });
+    if (!deletedCoupon) {
+      return res.status(404).send({
+        error: true,
+        message: "Coupon already deleted or doesn't exist",
+      });
+    }
+    res.status(201).send(deletedCoupon);
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(400).send({
+        error: true,
+        message: "Invalid coupon id",
+      });
+    }
+    res.status(500).send({ error: true, message: "Internal Server Error" });
+  }
+};
--- src/routes/couponsRoutes.js
+++ src/routes/couponsRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const couponController = require("../controllers/couponsController");
 
 router.post("/coupons", couponController.addNewCoupon);
+router.delete("/coupons/:couponId", couponController.deleteCoupon);
 
 module.exports = router;
